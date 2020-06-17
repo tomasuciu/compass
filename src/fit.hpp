b53073d84cfa9c2a73d90745bb3bc637525b0b52
Toma Suciu <tomasuciu@gmail.com>
2020-06-17 13:01:01 -0400

    Implement unoptimized Pratt fit

--- src/fit.hpp
+++ src/fit.hpp
@@ -71,7 +71,64 @@ static void Kasa(const Eigen::Matrix<double, 2, Eigen::Dynamic, Eigen::RowMajor>
 }
 
 
-static void Pratt(const Eigen::Ref<Eigen::Matrix2<T>>& data) {}
+static void Pratt(const Eigen::Matrix<double, 2, Eigen::Dynamic, Eigen::RowMajor>& data) {
+    auto mean = Eigen::Vector2<T>(data.row(0).mean(), data.row(1).mean());
+    auto colwiseData = data.colwise();
+
+    auto Mxx=0.0, Myy=0.0, Mxy=0.0, Mxz=0.0, Myz=0.0, Mzz=0.0;
+
+    std::for_each(colwiseData.begin(), colwiseData.end(), [&](const auto &column) {
+        auto Xi = column(0) - mean(0);
+        auto Yi = column(1) - mean(1);
+        auto Zi = std::pow(Xi, 2) + std::pow(Yi, 2);
+
+        Mxx += std::pow(Xi, 2);
+        Myy += std::pow(Yi, 2);
+        Mzz += std::pow(Zi, 2);
+        Mxy += Xi*Yi;
+        Mxz += Xi*Zi;
+        Myz += Yi*Zi;
+    });
+
+    int n = data.size();
+    Mxx /= n;
+    Myy /= n;
+    Mxy /= n;
+    Mxz /= n;
+    Myz /= n;
+    Mzz /= n;
+
+    // computing coefficients of characteristic polynomial
+    auto Mz = Mxx + Myy;
+    auto Cov_xy = Mxx*Myy - std::pow(Mxy, 2);
+    auto Var_z = Mzz - std::pow(Mz, 2);
+
+    auto A2 = 4.0*Cov_xy - 3.0*Mz*Mz - Mzz;
+    auto A1 = Var_z*Mz + 4.0*Cov_xy*Mz - Mxz*Mxz - Myz*Myz;
+    auto A0 = Mxz*(Mxz*Myy - Myz*Mxy) + Myz*(Myz*Mxx - Mxz*Mxy) - Var_z*Cov_xy;
+    auto A22 = A2 + A2;
+
+    int iter = 0;
+    double x=0.0, y=0.0;
+    for(x=0.0, y=0.0, iter=0; iter < 99; iter++) {
+
+        double Dy = A1 + x*(A22 + 16.0 * std::pow(x, 2));
+        double xnew = x - y/Dy;
+        if ((xnew == x)||(!std::isfinite(xnew))) break;
+        double ynew = A0 + xnew*(A1 + xnew*(A2 + 4.0*xnew*xnew));
+        if (abs(ynew)>=abs(y))  break;
+        x = xnew;  y = ynew;
+    }
+
+    auto DET = x*x - x*Mz + Cov_xy;
+    auto Xcenter = (Mxz*(Myy - x) - Myz*Mxy)/DET/2.0;
+    auto Ycenter = (Myz*(Mxx - x) - Mxz*Mxy)/DET/2.0;
+
+    std::cout << "X: " << Xcenter + mean(0) << std::endl;
+    std::cout << "Y: " << Ycenter + mean(1) << std::endl;
+    std::cout << "Radius: " << sqrt(Xcenter*Xcenter + Ycenter*Ycenter + Mz + x + x) << std::endl;
+
+    }
 
 };
 
