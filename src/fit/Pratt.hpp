b1abf5843ed6aecaa62fa3a84c9f28d70d5ce4c1
Toma Suciu <tomasuciu@gmail.com>
2020-07-23 16:42:24 -0400

    Begin work on PrattSVD; Form design matrix and compute thin SVD

--- src/fit/Pratt.hpp
+++ src/fit/Pratt.hpp
@@ -1,13 +1,35 @@
 #ifndef PRATT_HPP
 #define PRATT_HPP
 
+#include <Eigen/SVD>
 #include "../fit.hpp"
 
 namespace compass {
 
-class PrattSVD : public AlgebraicFit<PrattSVD> {};
+class PrattSVD : public AlgebraicFit<PrattSVD> {
+    public:
+        PrattSVD() : AlgebraicFit<PrattSVD>() {}
+        PrattSVD(Eigen::Ref<DataMatrixD> data) : AlgebraicFit<PrattSVD>(data) {}
+
+        PrattSVD& fit(Eigen::Ref<DataMatrixD> data) {
+            this->mean = center<double>(data);
+            Eigen::MatrixX<double> centered = data;
+
+            Eigen::VectorX<double> Z = (centered.array().square()).rowwise().sum();
+            DesignMatrix mat = (DesignMatrix(centered.rows(), 3)
+                    << centered, Eigen::VectorXd::Ones(centered.rows())).finished();
+
+            Eigen::BDCSVD<Eigen::MatrixX<double>> svd{mat, Eigen::ComputeThinU | Eigen::ComputeThinV};
+
+            return *this;
+        }
+};
+
 class PrattNewton : public AlgebraicFit<PrattNewton> {};
 
+// Possibly using regula falsi with Illinois modification (i.e., introduces quasi-regularization parameters to
+// scale function values according to iteration history)
+class PrattRobust : public AlgebraicFit<PrattRobust> {};
 }
 
 #endif
