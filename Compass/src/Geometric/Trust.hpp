60613c18d88246b05a15699bb6462f89a2da63fb
Toma Suciu <tomasuciu@gmail.com>
2020-09-18 17:05:38 -0400

    Implement unoptimized version of the Trust Region geometric algorithm

--- Compass/src/Geometric/Trust.hpp
+++ Compass/src/Geometric/Trust.hpp
@@ -23,12 +23,20 @@ class Trust : public GeometricFit<Trust<A>, A> {
             int nPar = 3;
 
             Eigen::RowVector3d par = initialGuess.getVector().transpose();
+
             auto [J, g, F] = computeStep(data, par);
 
             Eigen::VectorXd g0 = (Eigen::VectorXd(g.rows() + nPar)
                     << g, Eigen::VectorXd::Zero(nPar)).finished();
 
             for (int i = 0; i < IterMax; ++i) {
+                double progress = INT_MIN;
+
+                Eigen::MatrixXd JTemp;
+                Eigen::VectorXd gTemp;
+                Eigen::RowVector3d ParTemp;
+                double FTemp;
+
                 while(true) {
                     double lambda = 0.0001;
                     Eigen::MatrixXd lhs = (Eigen::MatrixXd(J.rows() + nPar, J.cols())
@@ -53,25 +61,78 @@ class Trust : public GeometricFit<Trust<A>, A> {
                         }
 
                         lambda = 0.01;
-
                         while(true) {
-                            // TODO
-                            // find lambda given delta
-                            lhs = (Eigen::MatrixXd(J.rows() + nPar, J.cols())
+                            Eigen::MatrixXd qr_lhs = (Eigen::MatrixXd(J.rows() + nPar, J.cols())
                                     << J, Eigen::Matrix3d::Identity().array()  * sqrt(lambda)).finished();
-                            Eigen::HouseholderQR<Eigen::MatrixXd> qr(lhs.rows(), lhs.cols());
-                            qr.compute(lhs);
+                            Eigen::ColPivHouseholderQR<Eigen::MatrixXd> qr(lhs.rows(), lhs.cols());
+                            qr.compute(qr_lhs);
 
-                            Eigen::MatrixXd q = qr.matrixQR();
+                            Eigen::MatrixXd Q = qr.matrixQ();
+                            Eigen::MatrixXd R = qr.matrixR();
+
+                            Eigen::MatrixXd Rinv = R.inverse();
+
+                            h = Rinv * (Q.transpose() * g0);
+                            double phi = h.norm() - Delta;
+
+                            if (std::abs(phi) < c1 * Delta) {
+                                break; // the subproblem is solved
+                            }
+
+                            Eigen::MatrixXd Rinvh = Rinv.transpose() * h;
+
+                            double phiprime = -1 * std::pow(Rinvh.norm(), 2)  / h.norm();
+                            double lambda_max_new = lambda;
+                            double lambda_min_new = lambda - (phi/phiprime);
+
+                            lambda = lambda - (phi + Delta) / Delta * (phi / phiprime);
+                            std::cout << lambda << std::endl;
+
+                            lambda = std::max(lambda, lambda_min);
+                            lambda = std::min(lambda, lambda_max);
+
+                            lambda_min = std::max(lambda_min, lambda_min_new);
+
+                            if (phi < 0) {
+                                lambda_max = lambda_max_new;
+                                break;
+                            }
                         }
-
-
                     }
+
+                    progress = h.norm() / (par(2) + EPSILON);
+                    if (progress < EPSILON)
+                        break;
+
+                    ParTemp = par - h.transpose();
+
+                    std::tie(JTemp, gTemp, FTemp) = computeStep(data, ParTemp);
+
+                    double ratio = (F - FTemp)/ (std::pow((J*h).norm(), 2) + std::pow((2*lambda * h.norm()), 2));
+
+                    if (ratio < c2 || ParTemp(2) < 0)
+                        Delta = std::min(Delta, h.norm()) / 2.0;
+                    if (ratio > c3 && ParTemp(2) > 0 && Delta < 2*h.norm())
+                        Delta = 2 * Delta;
+                    if (ratio > 0 && ParTemp(2) > 0)
+                        break;
                 }
+
+                if (progress < EPSILON)
+                    break;
+
+                par = ParTemp;
+                J = JTemp;
+                F = FTemp;
+                g = gTemp;
+
+                g0 = (Eigen::VectorXd(g.rows() + nPar) << g, Eigen::VectorXd::Zero(nPar)).finished();
             }
 
+            std::cout << par << std::endl;
             return *this;
         }
+
     private:
 
         std::tuple<Eigen::MatrixX<double>, Eigen::VectorX<double>, double>
